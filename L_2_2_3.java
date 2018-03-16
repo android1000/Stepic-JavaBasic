479eb46b2dd8dd20c536fb400f31f8b79c1e8949
Anatoly <epo_android@mail.ru>
2018-03-16 15:52:08 +0300

    add L_2_2_3

---
+++ L_2_2_3.java
@@ -0,0 +1,11 @@
+public class L_2_2_3 {
+
+    public static char charExpression(int a) {
+        return (char) ('\\' + a);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(charExpression(32));
+    }
+}
