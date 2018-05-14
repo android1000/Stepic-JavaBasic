958392283d9081cbc1f25594d64c096613fff203
Anatoly <epo_android@mail.ru>
2018-05-14 09:43:19 +0300

    add L_5_3_11

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (byte b : "Ы".getBytes(StandardCharsets.UTF_8)){
+            System.out.printf("%d ", Byte.toUnsignedInt(b));
+        }
     }
 }
