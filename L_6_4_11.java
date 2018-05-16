0ada5372bb5daf42c92e04f7d2298565adebbb5f
Anatoly <epo_android@mail.ru>
2018-05-16 18:24:45 +0300

    add L_6_4_12

--- L_6_4_11.java
+++ L_6_4_11.java
@@ -1,20 +1,15 @@
-import java.util.Random;
 import java.util.stream.IntStream;
 public class L_6_4_11 {
     public static void main(String[] args) {
         IntStream intStream = pseudoRandomStream(13);
-        intStream.limit(10)
+        System.out.println(
+                intStream.limit(10)
                 .peek(System.out::println)
-                .sum();
-        //System.out.println(mid(123456));
+                .sum()
+        );
     }
 
     public static IntStream pseudoRandomStream(int seed) {
         return IntStream.iterate(seed, x -> x * x / 10 % 1000);
     }
-
-    private static int mid(int i) {
-        return (i%10000)/10;
-    }
-
 }
